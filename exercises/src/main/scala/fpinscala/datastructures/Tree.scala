e5a592b3bad86e53f2741688aeecafcfc7681276
Alberto Rodriguez <albertorodriguez@paradigmatecnologico.com>
2014-10-22 09:02:36 +0200

    Exercise 3.25 resolved

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,8 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
-
-
-
-
+  def numberOfNodes[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(left, right) => 1 + numberOfNodes(left) + numberOfNodes(right)
+  }
 }
