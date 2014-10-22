96257fb7ea5abc8a52b07f12182fdbfe1eb08cad
Alberto Rodriguez <albertorodriguez@paradigmatecnologico.com>
2014-10-22 09:15:48 +0200

    Exercise 3.26 resolved

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -10,4 +10,15 @@ object Tree {
     case Leaf(_) => 1
     case Branch(left, right) => 1 + numberOfNodes(left) + numberOfNodes(right)
   }
+
+  def maximum(tree: Tree[Int]): Int = {
+    def findOutMaximum(tree: Tree[Int], maximum: Int): Int = {
+      tree match {
+        case Leaf(element) => element.max(maximum)
+        case Branch(left, right) => maximum.max(findOutMaximum(left, maximum)).max(findOutMaximum(right, maximum))
+      }
+    }
+
+    findOutMaximum(tree, 0)
+  }
 }
--- exercises/src/test/scala/fpinscala/datastructures/TreeTest.scala
+++ exercises/src/test/scala/fpinscala/datastructures/TreeTest.scala
@@ -3,23 +3,22 @@ package fpinscala.datastructures
 import org.scalatest.{Matchers, FlatSpec}
 
 class TreeTest extends FlatSpec with Matchers {
+  val leaf1 = Leaf(1)
+  val leaf2 = Leaf(2)
+  val leaf3 = Leaf(3)
+  val leaf4 = Leaf(4)
+  val leftChildBranch = Branch(leaf1, leaf2)
+  val rightChildBranch = Branch(leaf3, leaf4)
+  val tree = Branch(leftChildBranch, rightChildBranch)
+
 
   "Our tree implementation " should "count the number of elements" in {
-    val leaf1 = Leaf(1)
-    val leaf2 = Leaf(2)
-    val leaf3 = Leaf(3)
-    val leaf4 = Leaf(4)
-    val leftChildBranch = Branch(leaf1, leaf2)
-    val rightChildBranch = Branch(leaf3, leaf4)
-    val root = Branch(leftChildBranch, rightChildBranch)
-
-    val numberOfNodes = Tree.numberOfNodes(root)
+    val numberOfNodes = Tree.numberOfNodes(tree)
     numberOfNodes should be(7)
   }
 
-  /*it should "return and empty list when the list has one element" in {
-    val myList = List(1)
-    val myListWithoutFirstElement = List.tail(myList)
-    myListWithoutFirstElement should be(List())
-  }*/
+  it should "returns the maximum element in a Tree" in {
+    val maximumNumber = Tree.maximum(tree)
+    maximumNumber should be(4)
+  }
 }
