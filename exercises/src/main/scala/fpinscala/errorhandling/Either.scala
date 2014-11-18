026dec430a6af47e9d874fcbed9a6b59fa79671d
Alberto Rodriguez <albertorodriguez@paradigmatecnologico.com>
2014-11-18 08:55:36 +0100

    Exercise 4.6 resolved

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,7 +4,12 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = sys.error("todo")
+ def map[B](f: A => B): Either[E, B] = this match {
+   case Right(element) => Right(f(element))
+   case Left(ex) => Left(ex)
+ }
+
+
 
  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = sys.error("todo")
 
